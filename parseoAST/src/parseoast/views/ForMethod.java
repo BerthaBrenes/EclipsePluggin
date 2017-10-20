584e6d79a27eabdc6bf96d258164475c4890a8e4
Bamdres16 <brianm.bra.mm8@gmail.com>
2017-10-20 00:37:35 -0600

    Se añade método listener (Mouse)

--- parseoAST/src/parseoast/views/ForMethod.java
+++ parseoAST/src/parseoast/views/ForMethod.java
@@ -3,8 +3,13 @@ package parseoast.views;
 import java.util.concurrent.locks.Condition;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.MouseTrackListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.TouchEvent;
+import org.eclipse.swt.events.TouchListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.wb.swt.ResourceManager;
@@ -25,12 +30,14 @@ public class ForMethod extends genLabel {
 		label.setBackgroundImage(ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/decision-symbol.jpg"));
 		label.setBounds(getPosx(), getPosy(), 124, 60);
 		conditions();
+		label.addMouseListener(mouse(getText()));
 		return label;
 	}
 	public void conditions () {
 		Yes (getPosx()-100,getPosy()+80);
 		No(getPosx()+100,getPosy()+80);
 		getParent().addPaintListener(Lineas());
+		
 	}
 	public Label Yes (int x,int y) {
 		Label label = new Label(getParent(), SWT.NONE);
@@ -38,6 +45,7 @@ public class ForMethod extends genLabel {
 		label.setAlignment(SWT.CENTER);
 		label.setBackgroundImage(ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/action-process-symbol.png"));
 		label.setBounds(x, y, 124, 60);
+		label.addMouseListener(mouse(Yes));
 		return label;
 	}
 	public Label No (int x,int y) {
@@ -46,6 +54,7 @@ public class ForMethod extends genLabel {
 		label.setAlignment(SWT.CENTER);
 		label.setBackgroundImage(ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/action-process-symbol.png"));
 		label.setBounds(x, y, 124, 60);
+		label.addMouseListener(mouse(No));
 		return label;
 	}
 	public PaintListener Lineas () {
@@ -54,11 +63,36 @@ public class ForMethod extends genLabel {
 			@Override
 			public void paintControl(PaintEvent e) {
 				e.gc.drawLine(getPosx(), getPosy()+30, getPosx()-50, getPosy()+80);
+				e.gc.drawString("Si", getPosx()-25, getPosy()+40);
 				e.gc.drawLine(getPosx()+124, getPosy()+30, getPosx()+50+124, getPosy()+80);
-				
+				e.gc.drawString("No", getPosx()+174, getPosy()+40);
 			}
 		};
 		return pintar;
+	}
+	public MouseListener mouse (String text) {
+		MouseListener m = new MouseListener() {
+			
+			@Override
+			public void mouseUp(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(text);
+					
+			}
+		};
+		return m;
 	}
 
 }
