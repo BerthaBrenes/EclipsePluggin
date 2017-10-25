45b596771cfd834ca5e273b9fb2fc27368422872
Bamdres16 <brianm.bra.mm8@gmail.com>
2017-10-24 18:08:41 -0600

    MERGE

---
+++ parseoAST/src/parseoast/views/View.java
@@ -0,0 +1,29 @@
+package parseoast.views;
+
+import org.eclipse.gef.EditPartFactory;
+import org.eclipse.gef.RootEditPart;
+import org.eclipse.gef.editparts.FreeformGraphicalRootEditPart;
+import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+
+
+public class View extends ViewPart {
+	public View() {
+	}
+	
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+	}
+
+
+	@Override
+	public void setFocus() {
+	
+	}
+	
+}
