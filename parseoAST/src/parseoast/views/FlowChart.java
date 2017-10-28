de3519cd5ce73cedab6b6fecec6b7f67ae8fc9f7
Bamdres16 <brianm.bra.mm8@gmail.com>
2017-10-27 21:45:02 -0600

    ScrolledComposite, permite que se añadan más objetos V_H_SCROLL

--- parseoAST/src/parseoast/views/FlowChart.java
+++ parseoAST/src/parseoast/views/FlowChart.java
@@ -1,37 +1,37 @@
 package parseoast.views;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.draw2d.LightweightSystem;
-import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.events.MouseListener;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
-
-import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Path;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.wb.swt.ResourceManager;
 
-
-import MoViCo.*;
+import MoViCo.ActivityFigure;
 import listas.Lista;
 import parseoast.handlers.GetInfo;
-import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.ToolBar;
 
 public class FlowChart extends ViewPart{
 	public FlowChart() {
+		
 	}
 	public String[] array= new String[]{"Método1","Método2","Método3","Método4"};
 	private Lista<ActivityFigure> active = new Lista<>();
@@ -42,13 +42,43 @@ public class FlowChart extends ViewPart{
 	private Action action2;
 	private Action refresh;
 	private Combo combo;
+	private Canvas canvas;
+	
+	
+	public Canvas getCanvas() {
+		return canvas;
+	}
+	public void setCanvas(Canvas canvas) {
+		this.canvas = canvas;
+	}
 	public void createPartControl(Composite parent) {
 		
-		Canvas canvas = new Canvas(parent, SWT.NONE);
+		ScrolledComposite sc = new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+	    Composite composite = new Composite(sc, SWT.NONE);
+	    sc.setContent(composite);
+
+	    composite.setLayout(null);
+
+	    combo = new Combo(composite, SWT.NONE);
+	    Rectangle bou = parent.getBounds();
+	    combo.setBounds(new Rectangle(0, 0, 592, 23));
+	    
+	    
+	    for (int i = 0; i<20;i++) {
+	    	Label label = new Label(composite, SWT.NONE);
+	    	label.setText("Label: "+i);
+	    	label.setBounds(new Rectangle(posx, posy, 60, 20));
+	    	posy+=30;
+	    }
+	    sc.setExpandHorizontal(true);
+	    sc.setExpandVertical(true);
+	    sc.setMinSize(parent.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+        
 		
-		combo = new Combo(canvas, SWT.NONE);
-		combo.setBounds(0, 0, 594, 23);
 		
+    	
+    	
+    	
 		
 		
 		
@@ -56,47 +86,26 @@ public class FlowChart extends ViewPart{
         initializeToolBar();
 		
 }
-	public void getcontrol (Canvas canvas) {
-		LightweightSystem lws = new LightweightSystem(canvas);
-	    ChartFigure flowchart = new ChartFigure();
-	    lws.setContents(flowchart);
+	
+	
+	public static void drawArrow(GC gc, int x1, int y1, int x2, int y2, double arrowLength, double arrowAngle) {
+	    double theta = Math.atan2(y2 - y1, x2 - x1);
+	    double offset = (arrowLength - 2) * Math.cos(arrowAngle);
 
-	    TerminatorFigure start = new TerminatorFigure();
-	    start.setName("Metodo1");
-	    start.setBounds(new Rectangle(40, 20, 140, 20));
+	    gc.drawLine(x1, y1, (int)(x2 - offset * Math.cos(theta)), (int)(y2 - offset * Math.sin(theta)));
+
+	    Path path = new Path(gc.getDevice());
+	    path.moveTo((float)(x2 - arrowLength * Math.cos(theta - arrowAngle)), (float)(y2 - arrowLength * Math.sin(theta - arrowAngle)));
+	    path.lineTo((float)x2, (float)y2);
+	    path.lineTo((float)(x2 - arrowLength * Math.cos(theta + arrowAngle)), (float)(y2 - arrowLength * Math.sin(theta + arrowAngle)));
+	    path.close();
+
+	    gc.fillPath(path);
+
+	    path.dispose();
+	}
+
 	    
-	    DecisionFigure dec = new DecisionFigure();
-	    dec.setName("Should I?");
-	    dec.setBounds(new Rectangle(30, 60, 100, 60));
-	    
-	    ProcessFigure proc = new ProcessFigure();
-	    proc.setName("Do it!");
-	    proc.setBounds(new Rectangle(40, 180, 80, 40));
-	    TerminatorFigure stop = new TerminatorFigure();
-	    stop.setName("End");
-	    stop.setBounds(new Rectangle(40, 250, 80, 20));
-
-	    PathFigure path1 = new PathFigure();
-	    path1.setSourceAnchor(start.outAnchor);
-	    path1.setTargetAnchor(dec.inAnchor);
-	    PathFigure path2 = new PathFigure();
-	    path2.setSourceAnchor(dec.yesAnchor);
-	    path2.setTargetAnchor(proc.inAnchor);
-	    PathFigure path3 = new PathFigure();
-	    path3.setSourceAnchor(dec.noAnchor);
-	    path3.setTargetAnchor(stop.inAnchor);
-	    PathFigure path4 = new PathFigure();
-	    path4.setSourceAnchor(proc.outAnchor);
-	    path4.setTargetAnchor(stop.inAnchor);
-
-	    flowchart.add(start);
-	    flowchart.add(dec);
-	    flowchart.add(proc);
-	    flowchart.add(stop);
-	    flowchart.add(path1);
-	    flowchart.add(path2);
-	    flowchart.add(path3);
-	    flowchart.add(path4);
 
 	    
 		
@@ -105,11 +114,11 @@ public class FlowChart extends ViewPart{
 		
 	    	
 	    
-	}
+	
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
-		
+		combo.setFocus();
 	}
 	
 	public Canvas clean (Composite parent) {
@@ -134,7 +143,9 @@ public class FlowChart extends ViewPart{
     			@SuppressWarnings("static-access")
 				public void run () {
     				GetInfo nuevo = new GetInfo();
+    				
     				nuevo.setEleccion(combo.getText());
+    				nuevo.canvas = canvas;
     				try {
 						nuevo.execute(new ExecutionEvent());
 						
@@ -174,5 +185,6 @@ public class FlowChart extends ViewPart{
         toolbarManager.add(action2);
         toolbarManager.add(action);
         toolbarManager.add(action1);
+        
     }
 }
