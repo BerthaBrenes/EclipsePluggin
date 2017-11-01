5394bd9ee24471d2cb0b5ed7b8a99232879da792
Bamdres16 <brianm.bra.mm8@gmail.com>
2017-10-31 18:02:43 -0600

    Generar final de diagrama de flujo

--- parseoAST/src/parseoast/views/Fabrica_de_simbolos.java
+++ parseoAST/src/parseoast/views/Fabrica_de_simbolos.java
@@ -1,16 +1,28 @@
 package parseoast.views;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Path;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.wb.swt.ResourceManager;
 
 public class Fabrica_de_simbolos {
-	public PaintListener Process (int posx, int posy,Display display,String str) {
+	private Image decision = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/decision_symbol-60x46.PNG");
+	private Image process = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/action-process-symbol.png");
+	private Image start_end = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/start-end-process-symbol.png");
+	private Image f_decision = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/filled-decision_symbol-60x46.PNG");
+	private Image f_process = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/filled-action-process-symbol.png");
+	private Image f_start_end = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/filled-start-end-process-symbol.png");
+	
+	public PaintListener Process_Lineas (int posx, int posy,Display display) {
 		PaintListener listener = new PaintListener() {
 		Image image = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/start-end-process-symbol.png");
 			@Override
@@ -18,8 +30,7 @@ public class Fabrica_de_simbolos {
 				
 				// TODO Auto-generated method stub
 				
-				e.gc.drawImage(image, posx, posy);
-				e.gc.drawString(str, posx+10, posy+20, true);
+				
 				e.gc.setLineWidth(1);
 				e.gc.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
 				e.gc.setBackground(display.getSystemColor(SWT.COLOR_BLACK));
@@ -30,7 +41,7 @@ public class Fabrica_de_simbolos {
 		return listener;
 		
 }
-	public PaintListener For (int posx, int posy,Display display) {
+	public PaintListener For_Lineas (int posx, int posy,Display display) {
 		PaintListener listener = new PaintListener() {
 		Image image = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/decision_symbol-60x46.PNG");
 			@Override
@@ -49,6 +60,75 @@ public class Fabrica_de_simbolos {
 		};
 		return listener;
 }
+	public CLabel For (int posx,int posy,Composite parent, String name) {
+		CLabel label = new CLabel(parent, SWT.NONE);
+		label.setBackground(decision);
+		label.setBounds(posx,posy,110,65);
+		label.addPaintListener(new PaintListener() {
+				
+				@Override
+				public void paintControl(PaintEvent e) {
+					// TODO Auto-generated method stub
+					
+					e.gc.drawString(name, label.getBounds().width/3, label.getBounds().height/3,true);
+				}
+			});
+		label.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseUp(MouseEvent e) {
+				// TODO Auto-generated method stub
+				label.setBackground(decision);
+			}
+			
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// TODO Auto-generated method stub
+				label.setBackground(f_decision);
+			}
+			
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(name);
+			}
+		});
+		return label;
+	}
+	public CLabel Process (int posx,int posy,Composite parent, String name) {
+		CLabel label = new CLabel(parent, SWT.NONE);
+		label.setBackground(process);
+		label.setBounds(posx, posy, 110, 65);
+		label.addPaintListener(new PaintListener() {
+			
+			@Override
+			public void paintControl(PaintEvent e) {
+				// TODO Auto-generated method stub
+				e.gc.drawString(name, label.getBounds().width/3, label.getBounds().height/3,true);
+			}
+		});
+		label.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseUp(MouseEvent e) {
+				// TODO Auto-generated method stub
+				label.setBackground(process);
+			}
+			
+			@Override
+			public void mouseDown(MouseEvent e) {
+				// TODO Auto-generated method stub
+				label.setBackground(f_process);
+			}
+			
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(name);
+			}
+		});
+		return label;
+	}
 	public static void drawArrow(GC gc, int x1, int y1, int x2, int y2, double arrowLength, double arrowAngle) {
 	    double theta = Math.atan2(y2 - y1, x2 - x1);
 	    double offset = (arrowLength - 2) * Math.cos(arrowAngle);
--- parseoAST/src/parseoast/views/FlowChart.java
+++ parseoAST/src/parseoast/views/FlowChart.java
@@ -2,41 +2,25 @@ package parseoast.views;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.LightweightSystem;
-import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.events.ControlAdapter;
-import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Drawable;
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.GCData;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Path;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.wb.swt.ResourceManager;
 
-import MoViCo.ChartFigure;
-import MoViCo.DecisionFigure;
-import MoViCo.PathFigure;
-import MoViCo.TerminatorFigure;
 import listas.Lista;
 import parseoast.handlers.GetInfo;
-import org.eclipse.swt.custom.CCombo;
 
 public class FlowChart extends ViewPart{
 	/**
@@ -51,19 +35,28 @@ public class FlowChart extends ViewPart{
 	public String[] array= new String[]{"Método1","Método2","Método3","Método4"};
 
 	private Lista<PaintListener> active = new Lista<>();
+	private Lista<CLabel> labels = new Lista<>();
 	private int posx = 150;
 	private int posy = 50;
-	
+	private int bucle=0;
+	private int current =0;
 	private Action action;
 	private Action action1;
 	private Action action2;
 	private Action refresh;
 	
 	private Combo combo;
-	private Canvas canvas_1;
 	private Composite composite;
 	private ScrolledComposite sc;
 	private PaintListener listener;
+	private Label label;
+	private Image decision = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/decision_symbol-60x46.PNG");
+	private Image process = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/action-process-symbol.png");
+	private Image start_end = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/start-end-process-symbol.png");
+	private Image f_decision = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/filled-decision_symbol-60x46.PNG");
+	private Image f_process = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/filled-action-process-symbol.png");
+	private Image f_start_end = ResourceManager.getPluginImage("parseoAST", "Iconos de Diagrama/filled-start-end-process-symbol.png");
+	
 	/**
 	 * Se inicia al ejecutar el plugin, este contiene todos los widgets que deben abrirse al iniciar este
 	 * el parametro que recibe es el area donde se colocan cada widget
@@ -79,24 +72,16 @@ public class FlowChart extends ViewPart{
 	    
 	    composite = new Composite(sc, SWT.NONE);
 	    
+	    
+	    
 	    /**
 	     * Defect widgets in the part control
 	     */
 	    
-	  
 	    combo = new Combo(composite, SWT.NONE);
-	    combo.setBounds(124, 10, 330, 23);
-	    Label lblNewLabel = new Label(composite, SWT.NONE);
-	    lblNewLabel.setBounds(10, 13, 108, 15);
-	    lblNewLabel.setText("Select one method");
-	    
+	    combo.setBounds(10, 10, 330, 23);
 	    
 	   
-	
-	   
-	  
-	    
-	    
 	    /**
 	     * Final create part
 	     */
@@ -107,47 +92,7 @@ public class FlowChart extends ViewPart{
         initializeToolBar();
 		
 }
-	
-	
-	
-	public PaintListener paint2 () {
-		PaintListener listener = new PaintListener() {
-			
-			@Override
-			public void paintControl(PaintEvent e) {
-				
-				// TODO Auto-generated method stub
-				for (int i = 0; i<5;i++) {
-				e.gc.drawRectangle(160, i*120, 90, 90);
-				}
-				
-				
-			}
-		};
-		return listener;
-	}
-	/**
-	 * Metodo para dibujar lineas de flujo con una posicion en x y una posicion en y
-	 * @param gc
-	 * @param x1
-	 * @param y1
-	 * @param x2
-	 * @param y2
-	 * @param arrowLength
-	 * @param arrowAngle
-	 */
-	
 
-	    
-
-	    
-		
-		
-		
-		
-	    	
-	    
-	
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
@@ -159,22 +104,48 @@ public class FlowChart extends ViewPart{
 	 */
 	private void createActions() {
         // Create the actions
-    	{
+    	{   /**
+    		*Obtiene el paso siguiente
+    		*/	
     		action = new Action("Step Into") {
-    			public void run () {	
-    				
+    			Color red = new Color(composite.getDisplay(), 255,0,0);
+    			public void run () {
+    				if (bucle <= labels.Largo()) {
+    					if (bucle == current) {
+    						labels.Iterador(bucle).setForeground(red);
+    						System.out.println(labels.Iterador(bucle).getBackground());
+    						bucle++;
+    					}
+    					else if (current < bucle && bucle == labels.Largo()) {
+    						labels.Iterador(current).setForeground(null);
+    						bucle =0;
+    						current = 0;
+    					}
+    					else if (current < bucle) {
+    						labels.Iterador(current).setForeground(null);
+    						labels.Iterador(bucle).setForeground(red);
+    						bucle++;
+    						current++;
+    					}
+    					
+    				}
     			}
     		};
     		action.setImageDescriptor(ResourceManager.getPluginImageDescriptor("parseoAST", "icons/stepinto_co.png"));
-    		
+    		/**
+    		 * Obtiene el interior en un paso
+    		 */
     		action1 = new Action("Step Over") {
+    			
     			public void run () {
     				
     			}
     		};
     		//ejecuta el get info 
     		action1.setImageDescriptor(ResourceManager.getPluginImageDescriptor("parseoAST", "icons/stepover_co.png"));
-    		
+    		/**
+    		 * Inicia el proceso del diagrama
+    		 */
     		action2  = new Action ("Start Debug with Flowchart") {
     			@SuppressWarnings("static-access")
 				public void run () {
@@ -182,15 +153,20 @@ public class FlowChart extends ViewPart{
     				Fabrica_de_simbolos simbolos = new Fabrica_de_simbolos();
     				
     				nuevo.setEleccion(combo.getText());
+    				CLabel label = simbolos.Process(posx, posy, composite,combo.getText());
+    				labels.Insertar(label);
     				
-    				listener = simbolos.Process(posx, posy,composite.getDisplay(),combo.getText());
-					 active.Insertar(listener);
-					   composite.addPaintListener(listener);
-					   composite.redraw();
-					   posy+=100;
-    				   sc.setMinSize(new Point(posx, posy));
-    				   sc.update();
-    				   action2.setEnabled(false);
+    				
+    				listener = simbolos.Process_Lineas(posx, posy,composite.getDisplay());
+					active.Insertar(listener);
+					composite.addPaintListener(listener);
+					composite.redraw();
+					posy+=100;
+					sc.setMinSize(new Point(posx, posy));
+    				sc.update();
+    				action2.setEnabled(false);
+    				current = 0;
+    				bucle = 0;
     				try {
 						nuevo.execute(new ExecutionEvent());
 						
@@ -202,7 +178,11 @@ public class FlowChart extends ViewPart{
     			}
     		};
     		
+    		action2.setEnabled(false);
     		action2.setImageDescriptor(ResourceManager.getPluginImageDescriptor("parseoAST", "icons/resume_co.png"));
+    		/**
+    		 * Actualiza el combo, borra el contenido del composite
+    		 */
     		refresh = new Action("Refresh") {
     			public void run () {
     				GetInfo nuevo = new GetInfo();
@@ -221,10 +201,16 @@ public class FlowChart extends ViewPart{
 					for (int i = 0; i<active.Largo();i++) {
 	    				composite.removePaintListener(active.Iterador(i));
 	    				}
+					for (int i = 0; i<labels.Largo();i++) {
+						labels.Iterador(i).dispose();
+					}
 	    				posy = 50;
 	    				composite.redraw();
 	    				sc.setMinSize(new Point(posx, posy));
 	    				sc.update();
+	    				current = 0;
+	    				bucle = 0;
+	    				labels = new Lista<>();
     			}
 			};
 			refresh.setImageDescriptor(ResourceManager.getPluginImageDescriptor("parseoAST", "icons/refresh_nav.png"));
